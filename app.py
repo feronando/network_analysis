6f8f49704072a9827d486ef4bfbff881ecbcd951
Fernando Almeida <85702958+feronando@users.noreply.github.com>
2023-06-12 12:36:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ import streamlit.components.v1 as components
 
 st.title('The Marvel Universe Social Network')
 
-df_edges = pd.read_csv('https://raw.githubusercontent.com/feronando/network_analysis/main/hero-network.csv')
-G = nx.Graph()
-for row in df_edges.iterrows():
-  G.add_edge(row[1]["hero1"], row[1]["hero2"])
+# df_edges = pd.read_csv('https://raw.githubusercontent.com/feronando/network_analysis/main/hero-network.csv')
+# G = nx.Graph()
+# for row in df_edges.iterrows():
+#   G.add_edge(row[1]["hero1"], row[1]["hero2"])
 
-hero_network = Network(height="600px", width="100%", notebook=True, font_color="black", heading='Hero Network', cdn_resources='remote')
-hero_network.from_nx(G)
+# hero_network = Network(height="600px", width="100%", notebook=True, font_color="black", heading='Hero Network', cdn_resources='remote')
+# hero_network.from_nx(G)
 
-adj_list = hero_network.get_adj_list()
-for node in hero_network.nodes:
-  node["title"] = " Neighbors:<br>" + "<br>".join(adj_list[node["id"]])
-  node["value"] = len(adj_list[node["id"]])
+# adj_list = hero_network.get_adj_list()
+# for node in hero_network.nodes:
+#   node["title"] = " Neighbors:<br>" + "<br>".join(adj_list[node["id"]])
+#   node["value"] = len(adj_list[node["id"]])
 
-hero_network.show_buttons(filter_=['physics'])
-hero_network.show('hero_network.html')
+# hero_network.show_buttons(filter_=['physics'])
+# hero_network.show('hero_network.html')
 
 with open("hero_network.html", 'r', encoding='utf-8') as html_file:  
   source_code = html_file.read()
