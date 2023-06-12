7d33892a040c8fed12e1f9987116d075cea6d7a9
Fernando Almeida <85702958+feronando@users.noreply.github.com>
2023-06-12 07:39:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+from pyvis.network import Network
+import networkx as nx
+import pandas as pd
+from IPython.core.display import display, HTML
+import matplotlib.pyplot as plt
+import nxviz as nv
+import streamlit as st
+import streamlit.components.v1 as components
