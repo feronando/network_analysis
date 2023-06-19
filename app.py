2c0523c92882e5a43e3cbf822ebb39cb58df0db4
Fernando Almeida <85702958+feronando@users.noreply.github.com>
2023-06-19 15:22:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,30 @@ import matplotlib.pyplot as plt
 import nxviz as nv
 import streamlit as st
 import streamlit.components.v1 as components
+from urllib.parse import unquote
 
-st.title('The Marvel Universe Social Network')
+st.title('Wikispeedia Network')
 
-# df_edges = pd.read_csv('https://raw.githubusercontent.com/feronando/network_analysis/main/hero-network.csv')
-# G = nx.Graph()
-# for row in df_edges.iterrows():
-#   G.add_edge(row[1]["hero1"], row[1]["hero2"])
+df_articles = pd.read_csv('https://raw.githubusercontent.com/feronando/network_analysis/main/wikispeedia_paths-and-graph/articles.tsv', skiprows=11, names=["article"])
+df_links = pd.read_csv('https://raw.githubusercontent.com/feronando/network_analysis/main/wikispeedia_paths-and-graph/links.tsv', encoding='utf-8', sep="\t", skiprows=12, names=["linkSource", "linkTarget"])
 
-# hero_network = Network(height="600px", width="100%", notebook=True, font_color="black", heading='Hero Network', cdn_resources='remote')
-# hero_network.from_nx(G)
+wikispeedia_network = Network(height="600px", directed=True, width="100%", font_color="black", notebook=True, cdn_resources='remote')
 
-# adj_list = hero_network.get_adj_list()
-# for node in hero_network.nodes:
-#   node["title"] = " Neighbors:<br>" + "<br>".join(adj_list[node["id"]])
-#   node["value"] = len(adj_list[node["id"]])
+for row in df_articles.iterrows():
+  wikispeedia_network.add_node(unquote(row[1]["article"]))
+for row in df_links.iterrows():
+  wikispeedia_network.add_edge(unquote(row[1]["linkSource"]), unquote(row[1]["linkTarget"]))
 
-# hero_network.show_buttons(filter_=['physics'])
-# hero_network.show('hero_network.html')
+adj_list = wikispeedia_network.get_adj_list()
+for node in wikispeedia_network.nodes:
+  node["title"] = "Links: " + " || ".join(adj_list[node["id"]])
+  # node["degree"] = nx.degree(G, node["id"])
+  # node["in_degree"] = G.in_degree(node["id"])
+  # node["out_degree"] = G.out_degree(node["id"])
 
-with open("hero_network.html", 'r', encoding='utf-8') as html_file:  
+wikispeedia_network.show_buttons(filter_=['physics'])
+wikispeedia_network.show('wikispeedia.html')
+
+with open("wikispeedia.html", 'r', encoding='utf-8') as html_file:  
   source_code = html_file.read()
   components.html(source_code, height = 1200,width=1000)
